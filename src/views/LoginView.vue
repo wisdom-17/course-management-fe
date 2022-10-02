d9b8203a4da158b616aa552cd894c6d349f0d1db
Saeed <saeed17@gmail.com>
2022-10-02 12:12:37 +0100

    Basic login form
    emai and password fields

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,9 +1,21 @@
 <template>
   <h1>Login</h1>
-  <Dialog v-model:visible="display">Hello World</Dialog>
+  <form>
+    <span class="p-float-label">
+      <InputText id="email" type="text" v-model="email" />
+      <label for="email">Email</label>
+    </span>
+    <span class="p-float-label">
+      <Password id="password" v-model="password" :feedback="false" toggleMask />
+      <label for="password">Password</label>
+    </span>
+  </form>
 </template>
 <script setup>
 import { ref } from 'vue'
-// const props = defineProps(['display'])
-const display = ref(true)
+import InputText from 'primevue/inputtext'
+import Password from 'primevue/password'
+
+const email = ref()
+const password = ref()
 </script>
