b40c38a19ce3ba3f156f6915fe2bf78a45eb9de2
Saeed <saeed17@gmail.com>
2022-10-03 09:34:57 +0100

    CSS font style. Styling for login page.

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,20 +1,34 @@
 <template>
-  <h1>Login</h1>
-  <form>
-    <span class="p-float-label">
-      <InputText id="email" type="text" v-model="email" />
-      <label for="email">Email</label>
-    </span>
-    <span class="p-float-label">
-      <Password id="password" v-model="password" :feedback="false" toggleMask />
-      <label for="password">Password</label>
-    </span>
-  </form>
+  <div class="card h-screen flex justify-content-center align-items-center">
+    <form>
+      <div class="field mt-4 grid">
+        <span class="p-float-label">
+          <InputText id="email" type="text" v-model="email" />
+          <label for="email">Email</label>
+        </span>
+      </div>
+      <div class="field mt-4 grid">
+        <span class="p-float-label">
+          <Password
+            id="password"
+            v-model="password"
+            :feedback="false"
+            toggleMask
+          />
+          <label for="password">Password</label>
+        </span>
+      </div>
+      <div class="field grid mt-4">
+        <Button label="Login" />
+      </div>
+    </form>
+  </div>
 </template>
 <script setup>
 import { ref } from 'vue'
 import InputText from 'primevue/inputtext'
 import Password from 'primevue/password'
+import Button from 'primevue/button'
 
 const email = ref()
 const password = ref()
