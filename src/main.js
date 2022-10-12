f853273f0ac01d959071b8216d08b5206f248639
Saeed <saeed17@gmail.com>
2022-10-12 15:36:33 +0100

    Basic login functionality
    - Save successfully logged in user to Pinia store
    - Redirect to home route on successful login

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import PrimeVue from 'primevue/config'
+import { createPinia } from 'pinia'
 
 import './assets/main.css'
 import 'primevue/resources/themes/saga-blue/theme.css' //theme
@@ -10,8 +11,10 @@ import 'primeicons/primeicons.css' //icons
 import 'primeflex/primeflex.css'
 
 const app = createApp(App)
+const pinia = createPinia()
 
 app.use(router)
 app.use(PrimeVue, { ripple: true })
+app.use(pinia)
 
 app.mount('#app')
