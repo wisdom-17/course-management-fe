4bc3a3b637217939aee2238a1372f686a4d7c0a6
Saeed <saeed17@gmail.com>
2022-10-12 23:48:56 +0100

    Display login form validation error messages

---
+++ src/components/ErrorMessage.vue
@@ -0,0 +1,10 @@
+<template>
+  <Message severity="error">{{ message }}</Message>
+</template>
+
+<script setup>
+import Message from 'primevue/message'
+
+// eslint-disable-next-line no-unused-vars
+const props = defineProps({ message: String })
+</script>
