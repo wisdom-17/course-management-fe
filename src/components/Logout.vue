7775a9a9d9a80282f208d4f8facbdb6a4b588f12
Saeed <saeed17@gmail.com>
2022-10-14 21:57:10 +0100

    Style logout link
    Use Primevue button and icon to style logout link

--- src/components/Logout.vue
+++ src/components/Logout.vue
@@ -1,7 +1,15 @@
-<template> | <a href="#" @click="onClickLogout">Logout</a> </template>
+<template>
+  <Button
+    @click="onClickLogout"
+    label="Logout"
+    class="p-button-text p-button-plain"
+    icon="pi pi-power-off"
+  />
+</template>
 
 <script setup>
 import { useAuthStore } from '@/stores/auth'
+import Button from 'primevue/button'
 
 const storeAuth = useAuthStore()
 
