05d7b7f7866adf6f51dca035d9698a1904c1de21
Saeed <saeed17@gmail.com>
2022-10-20 10:22:01 +0100

    Basic toolbar with 'new' button

---
+++ src/components/courses/Toolbar.vue
@@ -0,0 +1,12 @@
+<template>
+  <Toolbar>
+    <template #start>
+      <Button icon="pi pi-plus" label="New" />
+    </template>
+  </Toolbar>
+</template>
+
+<script setup>
+import Toolbar from 'primevue/toolbar'
+import Button from 'primevue/button'
+</script>
--- src/views/CoursesView.vue
+++ src/views/CoursesView.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="courses">
     <h1>Courses</h1>
+    <Toolbar />
   </div>
 </template>
+
+<script setup>
+import Toolbar from '@/components/courses/Toolbar.vue'
+</script>
