3de22eccc286795aa650b452161330ecad6cbe7c
Saeed <saeed17@gmail.com>
2022-10-25 15:40:28 +0100

    Refactor Axios client into separate file

--- src/services/AuthService.js
+++ src/services/AuthService.js
@@ -1,63 +1,35 @@
-import axios from 'axios'
-import { useAuthStore } from '@/stores/auth'
-
-const apiURL = import.meta.env.VITE_BASE_API_URL
-
-export const authClient = axios.create({
-  baseURL: apiURL,
-  withCredentials: true, // required to handle the CSRF token
-})
-
-/*
- * Add a response interceptor
- */
-authClient.interceptors.response.use(
-  (response) => {
-    return response
-  },
-  function (error) {
-    const storeAuth = useAuthStore()
-    if (
-      error.response &&
-      [401, 419].includes(error.response.status) &&
-      storeAuth.loggedInUser
-    ) {
-      storeAuth.logout()
-    }
-    return Promise.reject(error)
-  }
-)
+import { axiosClient } from '@/services/Axios'
 
 export default {
   async login(payload) {
-    await authClient.get('/sanctum/csrf-cookie')
-    return authClient.post('/login', payload)
+    await axiosClient.get('/sanctum/csrf-cookie')
+    return axiosClient.post('/login', payload)
   },
   logout() {
-    return authClient.post('/logout')
+    return axiosClient.post('/logout')
   },
   async forgotPassword(payload) {
-    await authClient.get('/sanctum/csrf-cookie')
-    return authClient.post('/forgot-password', payload)
+    await axiosClient.get('/sanctum/csrf-cookie')
+    return axiosClient.post('/forgot-password', payload)
   },
   getAuthUser() {
-    return authClient.get('/api/users/auth')
+    return axiosClient.get('/api/users/auth')
   },
   async resetPassword(payload) {
-    await authClient.get('/sanctum/csrf-cookie')
-    return authClient.post('/reset-password', payload)
+    await axiosClient.get('/sanctum/csrf-cookie')
+    return axiosClient.post('/reset-password', payload)
   },
   updatePassword(payload) {
-    return authClient.put('/user/password', payload)
+    return axiosClient.put('/user/password', payload)
   },
   async registerUser(payload) {
-    await authClient.get('/sanctum/csrf-cookie')
-    return authClient.post('/register', payload)
+    await axiosClient.get('/sanctum/csrf-cookie')
+    return axiosClient.post('/register', payload)
   },
   sendVerification(payload) {
-    return authClient.post('/email/verification-notification', payload)
+    return axiosClient.post('/email/verification-notification', payload)
   },
   updateUser(payload) {
-    return authClient.put('/user/profile-information', payload)
+    return axiosClient.put('/user/profile-information', payload)
   },
 }
