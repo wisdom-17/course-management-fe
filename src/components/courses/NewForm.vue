93898dc67672f6c324674229b777c836b7fdf928
Saeed <saeed17@gmail.com>
2022-10-25 16:25:34 +0100

    Call save new course endpoint + display validation errors

--- src/components/courses/NewForm.vue
+++ src/components/courses/NewForm.vue
@@ -2,17 +2,30 @@
   <Card class="mt-4 surface-ground">
     <template #title>New Course</template>
     <template #content>
+      <ErrorMessage
+        :message="validation.message"
+        v-show="showErrorMessage"
+        @close="onClickClose"
+      />
       <form>
         <div class="field">
           <label for="courseName">Course Name</label>
           <InputText
             id="courseName"
             class="w-full"
+            :class="{ 'p-invalid': validation.errors.name.length > 0 }"
             type="text"
-            v-model="courseName"
+            v-model="course.name"
           />
+          <small class="p-error">{{ validation.errors.name[0] }}</small>
         </div>
-        <Button label="Submit" @click="handleOnClickSubmitButton()" />
+        <Button
+          class="mr-2"
+          label="Save"
+          @click="handleOnClickSubmitButton()"
+        />
+        <Button class="mr-2" label="Next Step" />
+        <Button label="Cancel" />
       </form>
     </template>
   </Card>
@@ -21,14 +34,41 @@
 <script setup>
 import Button from 'primevue/button'
 import Card from 'primevue/card'
+import CourseService from '@/services/Course'
 import InputText from 'primevue/inputtext'
-
+import ErrorMessage from '@/components/ErrorMessage.vue'
 import { ref } from 'vue'
 
-const courseName = ref('')
+const course = ref({ name: '' })
+const validation = ref({ message: '', errors: { name: [] } })
+const showErrorMessage = ref(false)
 
-const handleOnClickSubmitButton = () => {
-  console.log('clicked submit button')
+const handleOnClickSubmitButton = async () => {
+  const payload = { name: course.value.name }
+
+  // clear validation errors
+  validation.value.message = ''
+  validation.value.errors = { name: [] }
+
+  try {
+    const apiResult = await CourseService.new(payload)
+    console.log(apiResult)
+  } catch (error) {
+    validation.value.message = error.response.data.message
+    // update validation error messages with ones
+    // returned from API call
+    validation.value.errors = {
+      ...validation.value.errors,
+      ...error.response.data.errors,
+    }
+    showErrorMessage.value = true
+    console.log(error)
+  }
+}
+
+const onClickClose = () => {
+  validation.value.message = ''
+  showErrorMessage.value = false
 }
 </script>
 <style scoped>
