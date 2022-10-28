fb69ba4a3de2453cc62d8411f961342734da2e1e
Saeed <saeed17@gmail.com>
2022-10-28 22:47:12 +0100

    Refactor date range picker into separate component

---
+++ src/components/DateRangePicker.vue
@@ -0,0 +1,52 @@
+<template>
+  <label for="dateRange">Start and End Dates</label>
+  <Calendar
+    id="dateRange"
+    :class="{ 'p-invalid': validationErrorMessages.length > 0 }"
+    class="mr-1 w-4"
+    v-model="selectedDateRange"
+    selectionMode="range"
+    dateFormat="dd/mm/yy"
+    :showButtonBar="true"
+    :showIcon="true"
+    @date-select="onSelectedDateRange"
+  />
+  <template v-for="errorMessage in validationErrorMessages" :key="errorMessage">
+    <small class="p-error">
+      {{ errorMessage }}
+    </small>
+    {{ ' ' }}
+  </template>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import Calendar from 'primevue/calendar'
+
+const emit = defineEmits(['selectedDateRange'])
+const selectedDateRange = ref([])
+
+const onSelectedDateRange = () => {
+  // emit event only after two dates (i.e. range) have been selected and formatted
+  if (selectedDateRange.value.filter((elem) => !!elem).length === 2) {
+    const formattedStartDate = new Date(
+      selectedDateRange.value[0]
+    ).toLocaleDateString('fr-CA') // Canadian date format is yyyy-mm-dd 
+
+    const formattedEndDate = new Date(
+      selectedDateRange.value[1]
+    ).toLocaleDateString('fr-CA')
+
+    emit('selectedDateRange', [formattedStartDate, formattedEndDate])
+  }
+}
+
+defineProps({
+  validationErrorMessages: {
+    type: Array,
+    default() {
+      return []
+    },
+  },
+})
+</script>
