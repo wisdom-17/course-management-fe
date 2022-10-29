f5d55381b64350d1a14dd0407b80bb055e3d3231
Saeed <saeed17@gmail.com>
2022-10-29 18:33:24 +0100

    Datatable for courses

--- src/views/CoursesView.vue
+++ src/views/CoursesView.vue
@@ -2,9 +2,47 @@
   <div class="col courses">
     <h1>Courses</h1>
     <Toolbar />
+    <DataTable :value="courses" class="mt-4">
+      <Column
+        v-for="col in columns"
+        :field="col.field"
+        :header="col.header"
+        :key="col.field"
+      ></Column>
+    </DataTable>
   </div>
 </template>
 
 <script setup>
+import CourseService from '@/services/Course'
+import DataTable from 'primevue/datatable'
+import Column from 'primevue/column'
+import { onMounted, ref } from 'vue'
 import Toolbar from '@/components/courses/Toolbar.vue'
+
+const courses = ref([])
+const columns = ref([
+  { field: 'name', header: 'Name' },
+  { field: 'startDate', header: 'Start Date' },
+  { field: 'endDate', header: 'End Date' },
+  { field: 'teachingDays', header: 'Teaching Days' },
+  { field: 'createdAt', header: 'Created At' },
+  { field: 'updatedAt', header: 'Updated At' },
+])
+
+onMounted(() => {
+  console.log('the component is now mounted')
+  CourseService.list().then((data) => {
+
+    // const formattedCoursesData = data.data.map(obj => {
+    //   { 
+    //     name: obj.name,
+    //     startDate:
+    //   }
+    // })
+    console.log(data)
+    courses.value = data.data
+    console.log(courses.value)
+  })
+})
 </script>
