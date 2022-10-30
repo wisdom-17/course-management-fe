5bbcda187397866549e849de2642cb85cb58759e
Saeed <saeed17@gmail.com>
2022-10-30 10:35:24 +0000

    Refactor courses table into seperate component

---
+++ src/components/courses/Table.vue
@@ -0,0 +1,29 @@
+<template>
+  <DataTable :value="courses">
+    <Column
+      v-for="col in columns"
+      :field="col.field"
+      :header="col.header"
+      :key="col.field"
+    ></Column>
+  </DataTable>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import DataTable from 'primevue/datatable'
+import Column from 'primevue/column'
+
+defineProps({
+  courses: Array,
+})
+
+const columns = ref([
+  { field: 'name', header: 'Name' },
+  { field: 'startDate', header: 'Start Date' },
+  { field: 'endDate', header: 'End Date' },
+  { field: 'teachingDays', header: 'Teaching Days' },
+  { field: 'createdAt', header: 'Created At' },
+  { field: 'updatedAt', header: 'Updated At' },
+])
+</script>
