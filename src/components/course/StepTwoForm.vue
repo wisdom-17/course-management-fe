1eb9b07f3b5da27aa936ee6542720192b9b839c1
Saeed <saeed17@gmail.com>
2022-11-05 15:01:27 +0000

    Button + functionality to add additional daterangepickers.
    Disable already selected dates in daterange picker

--- src/components/course/StepTwoForm.vue
+++ src/components/course/StepTwoForm.vue
@@ -12,16 +12,32 @@
         @close="onClickCloseSuccessMessage"
       />
       <form class="newCourse">
+        <template v-for="(term, index) in terms" :key="index">
+          <div class="field">
+            <DateRangePicker
+              :label="`Term ${term} Start and End Dates`"
+              :disabledDates="
+                formattedSelectedDates
+                  .filter((obj, rangeIndex) => index !== rangeIndex)
+                  .flat()
+              "
+              :validationErrorMessages="[
+                ...validation.errors.startDate,
+                ...validation.errors.endDate,
+              ]"
+              @selected-date-range="
+                (selectedDateRange) => (termDates[index] = selectedDateRange)
+              "
+              @cleared-date-range="() => (termDates[index] = [])"
+            />
+          </div>
+        </template>
         <div class="field">
-          <DateRangePicker
-            label="Term 1 Start and End Dates"
-            :validationErrorMessages="[
-              ...validation.errors.startDate,
-              ...validation.errors.endDate,
-            ]"
-            @selected-date-range="
-              (selectedDateRange) => (course.dateRange = selectedDateRange)
-            "
+          <Button
+            class="p-button-sm"
+            label="Additional Terms"
+            icon="pi pi-plus"
+            @click="onClickAdditionalTermsButton"
           />
         </div>
 
@@ -30,7 +46,6 @@
           @next-button-clicked="onClickNextButton"
           @previous-button-clicked="onClickPreviousButton"
           :hasNextButton="true"
-          :hasPreviousButton="true"
         />
       </form>
     </template>
@@ -38,40 +53,65 @@
 </template>
 
 <script setup>
+import { ref, computed } from 'vue'
+import { useRouter } from 'vue-router'
+import Button from 'primevue/button'
 import Card from 'primevue/card'
 import CourseService from '@/services/Course'
 import DateRangePicker from '@/components/DateRangePicker.vue'
 import ErrorMessage from '@/components/ErrorMessage.vue'
 import MultiStepFormButtons from '@/components/course/MultiStepFormButtons.vue'
 import SuccessMessage from '@/components/SuccessMessage.vue'
-import { ref, computed } from 'vue'
-import { useRouter } from 'vue-router'
 
 const router = useRouter()
 
-const course = ref({ name: '', dateRange: [], teachingDays: [] })
+const termDates = ref([])
+const terms = ref([1])
 const validation = ref({
   message: '',
   errors: { name: [], startDate: [], endDate: [], teachingDays: [] },
 })
-
 const successMessage = ref([])
+
+// formattedSelectedDates is used to disable selected
+// dates in daterange pickers for other terms (i.e. cannot
+// select date already selected in another term)
+const formattedSelectedDates = computed(() => {
+  return termDates.value.map((dates) => {
+    if (dates.length === 2) {
+      const startDate = new Date(dates[0].replace(/-/g, '/'))
+      const endDate = new Date(dates[1].replace(/-/g, '/'))
+      return getDatesInRange(startDate, endDate)
+    }
+    return []
+  })
+})
+
+// helper/utility method to get all dates within a range
+const getDatesInRange = (startDate, endDate) => {
+  const date = new Date(startDate.getTime())
+  const dates = []
+
+  while (date <= endDate) {
+    dates.push(new Date(date))
+    date.setDate(date.getDate() + 1)
+  }
+
+  return dates
+}
 
 const showErrorMessage = computed(() => {
   return validation.value.message !== ''
 })
-
 const showSuccessMessage = computed(() => {
   return successMessage.value.length !== 0
 })
 
 const onClickSubmitButton = async () => {
   const payload = {
-    name: course.value.name,
-    startDate: course.value.dateRange[0],
-    endDate: course.value.dateRange[1],
-    teachingDays: course.value.teachingDays,
+    terms: termDates.value,
   }
+  console.log(payload)
 
   // clear validation errors
   validation.value.message = ''
@@ -102,8 +142,14 @@ const onClickSubmitButton = async () => {
     }
   }
 }
+
 const onClickNextButton = async () => {
   router.push({ name: 'courseStepThree' })
+}
+
+const onClickAdditionalTermsButton = () => {
+  const termNumber = terms.value.length + 1
+  terms.value.push(termNumber)
 }
 
 const onClickPreviousButton = async () => {
