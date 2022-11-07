aa8564c76734731b7a1ce5fc8bc26bf329105077
Saeed <saeed17@gmail.com>
2022-11-07 14:35:57 +0000

    POST endpoint for saving new dates for course

--- src/services/Course.js
+++ src/services/Course.js
@@ -4,6 +4,9 @@ export default {
   async new(payload) {
     return axiosClient.post('/api/courses', payload)
   },
+  async newDates(payload, courseId) {
+    return axiosClient.post(`/api/courses/${courseId}/dates`, payload)
+  },
   list() {
     return axiosClient.get('/api/courses')
   },
