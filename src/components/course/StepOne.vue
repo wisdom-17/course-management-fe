27299757eedd57154af30516c5f1c14654bd441e
Saeed <saeed17@gmail.com>
2022-11-07 18:53:33 +0000

    Refactor new course form into seperate component. Rename step components.

---
+++ src/components/course/StepOne.vue
@@ -0,0 +1,18 @@
+<template>
+  <Card class="mt-4 surface-ground w-auto">
+    <template #content>
+      <CourseForm @save-success="emitSaveSuccessEvent" />
+    </template>
+  </Card>
+</template>
+
+<script setup>
+import Card from 'primevue/card'
+import CourseForm from '@/components/course/CourseForm.vue'
+
+const emit = defineEmits(['saveSuccess'])
+
+const emitSaveSuccessEvent = (message) => {
+  emit('saveSuccess', message)
+}
+</script>
