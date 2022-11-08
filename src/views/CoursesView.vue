3be5a62ff55513dcff1884cb1ba9e8cdd8abdb74
Saeed <saeed17@gmail.com>
2022-11-08 22:25:09 +0000

    Loading spinner course list table

--- src/views/CoursesView.vue
+++ src/views/CoursesView.vue
@@ -2,7 +2,7 @@
   <div class="col courses">
     <h1>Courses</h1>
     <Toolbar />
-    <Table :courses="courses" class="mt-4" />
+    <Table :courses="courses" class="mt-4" :loading="loading" />
   </div>
 </template>
 
@@ -13,10 +13,13 @@ import Table from '@/components/course/Table.vue'
 import Toolbar from '@/components/course/Toolbar.vue'
 
 const courses = ref([])
+const loading = ref(false)
 
 onMounted(() => {
+  loading.value = true
   CourseService.list().then((data) => {
     courses.value = data.data
+    loading.value = false
   })
 })
 </script>
