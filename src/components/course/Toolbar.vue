6cc325ef5c6fb5123c3c69c77520f1065ac9bc6f
Saeed <saeed17@gmail.com>
2022-11-09 17:35:47 +0000

    Delete button (disabled + active functionality)

--- src/components/course/Toolbar.vue
+++ src/components/course/Toolbar.vue
@@ -1,7 +1,19 @@
 <template>
   <Toolbar>
     <template #start>
-      <Button @click="handleNewButtonClick()" icon="pi pi-plus" label="New" />
+      <Button
+        class="mr-2"
+        @click="onClickNewButton()"
+        icon="pi pi-plus"
+        label="New"
+      />
+      <Button
+        class="p-button-danger"
+        @click="onClickDeleteButton()"
+        icon="pi pi-trash"
+        label="Delete"
+        :disabled="isDeleteButtonDisabled"
+      />
     </template>
   </Toolbar>
 </template>
@@ -11,14 +23,24 @@ import Toolbar from 'primevue/toolbar'
 import Button from 'primevue/button'
 import { useRouter } from 'vue-router'
 
+defineProps({
+  isDeleteButtonDisabled: {
+    type: Boolean,
+    default: true,
+  },
+})
 const router = useRouter()
 
-const handleNewButtonClick = () => {
+const onClickNewButton = () => {
   // for some reason, using route name doesn't
   // load the nested route component, in this case
   // /new-course/ should should show the step 1 course
   // details form
   // router.push({ name: 'newCourse' })
   router.push({ path: 'new-course' })
+}
+
+const onClickDeleteButton = () => {
+  console.log('onClickDeleteButton')
 }
 </script>
