a3a1df435b9277b02faec6e684d18e295ca2e77d
Saeed <saeed17@gmail.com>
2022-11-10 08:33:41 +0000

    Move courses retrieval to pinia store

--- src/views/CoursesView.vue
+++ src/views/CoursesView.vue
@@ -1,24 +1,21 @@
 <template>
   <div class="col courses">
     <h1>Courses</h1>
-    <Table :courses="courses" :loading="loading" />
+    <Table :courses="storeCourse.list" :loading="storeCourse.loading" />
   </div>
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue'
-import CourseService from '@/services/Course'
+import { onMounted } from 'vue'
 import Table from '@/components/course/Table.vue'
+import { useCourseStore } from '@/stores/course'
 
-const courses = ref([])
-const loading = ref(false)
+const storeCourse = useCourseStore()
 
 onMounted(() => {
-  loading.value = true
-  CourseService.list().then((data) => {
-    courses.value = data.data
-    loading.value = false
-  })
+  // check to prevent hammering the API unnecessarily
+  if (storeCourse.list.length === 0) {
+    storeCourse.getCourses()
+  }
 })
-
 </script>
