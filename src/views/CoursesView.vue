20deeac634fa6aa04482d2767a8804889f9aa522
Saeed <saeed17@gmail.com>
2022-11-10 09:35:05 +0000

    Move courses retrieval to table component

--- src/views/CoursesView.vue
+++ src/views/CoursesView.vue
@@ -1,21 +1,10 @@
 <template>
   <div class="col courses">
     <h1>Courses</h1>
-    <Table :courses="storeCourse.list" :loading="storeCourse.loading" />
+    <Table />
   </div>
 </template>
 
 <script setup>
-import { onMounted } from 'vue'
 import Table from '@/components/course/Table.vue'
-import { useCourseStore } from '@/stores/course'
-
-const storeCourse = useCourseStore()
-
-onMounted(() => {
-  // check to prevent hammering the API unnecessarily
-  if (storeCourse.list.length === 0) {
-    storeCourse.getCourses()
-  }
-})
 </script>
