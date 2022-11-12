5f3ded768e060fa6bf1ed695354d6b5b74745b7e
Saeed <saeed17@gmail.com>
2022-11-12 14:07:05 +0000

    Delete and edit split button for each row

--- src/components/course/Table.vue
+++ src/components/course/Table.vue
@@ -18,10 +18,10 @@
     ></Column>
     <Column>
       <template #body="slotProps">
-        <Button
+        <SplitButton
           icon="pi pi-pencil"
           class="p-button-rounded p-button-success mr-2"
-          @click="onClickEditButton(slotProps.data)"
+          :model="editMenuItems(slotProps.data)"
         />
         <Button
           icon="pi pi-trash"
@@ -37,6 +37,7 @@
 import { ref, onMounted } from 'vue'
 import DataTable from 'primevue/datatable'
 import Button from 'primevue/button'
+import SplitButton from 'primevue/splitbutton'
 import Column from 'primevue/column'
 import Toolbar from '@/components/course/Toolbar.vue'
 import { useCourseStore } from '@/stores/course'
@@ -52,18 +53,37 @@ const columns = ref([
   { field: 'teachingDays', header: 'Teaching Days' },
   { field: 'createdAt', header: 'Created At' },
   { field: 'updatedAt', header: 'Updated At' },
-  // { header: 'Actions' },
 ])
-
-const onClickEditButton = (rowData) => {
-  console.log('clicked edit button')
-  console.log(rowData)
-}
 
 const onClickDeleteButton = (rowData) => {
   console.log('clicked delete button')
   console.log(rowData)
 }
+
+const editMenuItems = (rowData) => [
+  {
+    label: 'Edit Course Details',
+    command: () => {
+      console.log(rowData)
+      console.log('clicked edit course details')
+      // console.log(currentRowData.value)
+    },
+  },
+  {
+    label: 'Edit Term Dates',
+    command: () => {
+      console.log('clicked edit term dates details')
+      console.log(rowData)
+    },
+  },
+  {
+    label: 'Edit Holiday Dates',
+    command: () => {
+      console.log('clicked edit holiday details')
+      console.log(rowData)
+    },
+  },
+]
 
 onMounted(() => {
   // check to prevent hammering the API unnecessarily
