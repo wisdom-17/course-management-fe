e4d5605767011469dc7aaef30bc4fccab81e6202
Saeed <saeed17@gmail.com>
2022-11-28 10:53:44 +0000

    Remove unneeded code

--- src/components/teacher/Toolbar.vue
+++ src/components/teacher/Toolbar.vue
@@ -12,7 +12,6 @@
         @click="onClickDeleteButton()"
         icon="pi pi-trash"
         label="Delete"
-        :disabled="selectedCourses.length === 0"
       />
     </template>
   </Toolbar>
@@ -21,27 +20,20 @@
 <script setup>
 import Toolbar from 'primevue/toolbar'
 import Button from 'primevue/button'
-import { useCourseStore } from '@/stores/course'
 import { useConfirm } from 'primevue/useconfirm'
 import { useDialog } from 'primevue/usedialog'
 import TeacherForm from '@/components/teacher/TeacherForm.vue'
 
-const storeCourse = useCourseStore()
 const confirm = useConfirm()
 const dialog = useDialog()
 
-const props = defineProps({
-  selectedCourses: {
-    type: Array,
-    default: () => [],
-  },
-})
-
-const emit = defineEmits(['deleteSuccess'])
-
 const onClickNewButton = () => {
   console.log('new button clicked')
-  dialog.open(TeacherForm)
+  dialog.open(TeacherForm, { props: {
+      style: { width: '60vw' },
+      header: 'New Teacher',
+    },
+  })
 }
 
 const onClickDeleteButton = async () => {
@@ -51,14 +43,7 @@ const onClickDeleteButton = async () => {
     header: 'Delete',
     icon: 'pi pi-trash',
     acceptClass: 'p-button-danger',
-    accept: () => {
-      const selectedCoursesIds = props.selectedCourses.map((obj) => obj.id)
-      // this event is used to clear the selected courses which
-      // in turn is used to determine the delete buttons active/disabled status
-      storeCourse.delete(selectedCoursesIds).then(() => {
-        emit('deleteSuccess')
-      })
-    },
+    accept: () => {},
   })
 }
 </script>
