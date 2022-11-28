b70b540e914a169dc30117451fbb12e80b557337
Saeed <saeed17@gmail.com>
2022-11-28 10:53:32 +0000

    Hourly pay field

--- src/components/teacher/TeacherForm.vue
+++ src/components/teacher/TeacherForm.vue
@@ -9,6 +9,17 @@
         v-model="teacher.name"
       />
     </div>
+    <div class="field">
+      <label for="hourlyPay">Hourly Pay</label>
+      <InputNumber
+        id="hourlyPay"
+        class="mr-1 w-6"
+        v-model="teacher.hourlyPay"
+        mode="currency"
+        currency="GBP"
+        locale="en-GB"
+      />
+    </div>
 
     <Button icon="pi pi-save" label="Save" />
   </form>
@@ -18,8 +29,10 @@
 import { ref } from 'vue'
 import Button from 'primevue/button'
 import InputText from 'primevue/inputtext'
+import InputNumber from 'primevue/inputnumber'
 
 const teacher = ref({
   name: '',
+  hourlyPay: 0,
 })
 </script>
