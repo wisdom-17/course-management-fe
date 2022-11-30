9c83b8f1a4d1db9dec2a395bd6f7bd7ec02d2204
Saeed <saeed17@gmail.com>
2022-11-30 08:51:49 +0000

    Remove batch delete functionality

--- src/components/teacher/Toolbar.vue
+++ src/components/teacher/Toolbar.vue
@@ -7,12 +7,6 @@
         icon="pi pi-plus"
         label="New"
       />
-      <Button
-        class="p-button-danger"
-        @click="onClickDeleteButton()"
-        icon="pi pi-trash"
-        label="Delete"
-      />
     </template>
   </Toolbar>
 </template>
@@ -20,11 +14,9 @@
 <script setup>
 import Toolbar from 'primevue/toolbar'
 import Button from 'primevue/button'
-import { useConfirm } from 'primevue/useconfirm'
 import { useDialog } from 'primevue/usedialog'
 import TeacherForm from '@/components/teacher/TeacherForm.vue'
 
-const confirm = useConfirm()
 const dialog = useDialog()
 
 const onClickNewButton = () => {
@@ -33,17 +25,6 @@ const onClickNewButton = () => {
       style: { width: '60vw' },
       header: 'New Teacher',
     },
-  })
-}
-
-const onClickDeleteButton = async () => {
-  confirm.require({
-    message:
-      'Are you sure you want to delete the selected course(s) with their term and holiday dates?',
-    header: 'Delete',
-    icon: 'pi pi-trash',
-    acceptClass: 'p-button-danger',
-    accept: () => {},
   })
 }
 </script>
