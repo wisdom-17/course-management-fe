55d4596a397756649a0c552c655f9a66e28e8c5b
Saeed <saeed17@gmail.com>
2023-03-06 07:26:45 +0000

    Delete term fields + semester field in term field section

--- src/components/courseCalendar/CourseCalendarForm.vue
+++ src/components/courseCalendar/CourseCalendarForm.vue
@@ -37,7 +37,12 @@
           <!-- we need the v-if="semester" check to ensure deleted semester fields are handled as expected-->
           <div v-if="semester" class="field">
             <label for="semesterName">{{ `Semester ${index + 1} Name` }}</label>
-            <InputText id="semesterName" class="mr-1 w-6" type="text" />
+            <InputText
+              id="semesterName"
+              class="mr-1 w-6"
+              type="text"
+              v-model="storeCourseCalendar.newForm.semesters[index].name"
+            />
             <small class="p-error"></small>
             <Button
               v-if="showDeleteSemesterButton"
@@ -58,26 +63,36 @@
       </Fieldset>
 
       <Fieldset legend="Terms" class="mt-4">
-        <div class="formgrid grid">
-          <div class="field col-3">
-            <label for="semester">Semester</label>
-            <Dropdown id="semester" class="w-full" />
-            <small class="p-error"></small>
+        <template
+          v-for="(term, index) in storeCourseCalendar.newForm.terms"
+          :key="index"
+        >
+          <div v-if="term" class="formgrid grid">
+            <div class="field col-3">
+              <label for="semester">Semester</label>
+              <Dropdown id="semester" class="w-full" :options="semesterNames" />
+              <small class="p-error"></small>
+            </div>
+            <div class="field col-3">
+              <label for="termName">{{ `Term ${index + 1} Name` }}</label>
+              <InputText id="termName" class="w-full" type="text" />
+              <small class="p-error"></small>
+            </div>
+            <div class="field col-4">
+              <DateRangePicker
+                label="Term Start and End Dates"
+                cssClass="w-8 mr-1"
+              />
+              <small class="p-error"></small>
+              <Button
+                v-if="showDeleteTermButton"
+                icon="pi pi-trash"
+                class="p-button-danger"
+                @click="onClickDeleteTermButton(index)"
+              />
+            </div>
           </div>
-          <div class="field col-3">
-            <label for="termName">Term 1 Name</label>
-            <InputText id="termName" class="w-full" type="text" />
-            <small class="p-error"></small>
-          </div>
-          <div class="field col-4">
-            <DateRangePicker
-              label="Term 1 Start and End Dates"
-              cssClass="w-8 mr-1"
-            />
-            <small class="p-error"></small>
-            <Button icon="pi pi-trash" class="p-button-danger" />
-          </div>
-        </div>
+        </template>
         <div class="field">
           <Button
             class="p-button-sm"
@@ -142,7 +157,10 @@ const onClickAdditionalSemestersButton = () => {
   storeCourseCalendar.newForm.semesters.push({ name: '' })
 }
 
-const onClickAdditionalTermsButton = () => console.log('clicked + term button')
+const onClickAdditionalTermsButton = () => {
+  console.log('clicked + term button')
+  storeCourseCalendar.newForm.terms.push({ name: '' })
+}
 const onClickAdditionalHolidaysButton = () =>
   console.log('clicked + holiday button')
 
@@ -151,6 +169,18 @@ const showDeleteSemesterButton = computed(() => {
     ? true
     : false
 })
+
+const showDeleteTermButton = computed(() => {
+  return storeCourseCalendar.newForm.terms.filter((x) => x).length > 1
+    ? true
+    : false
+})
+
+const semesterNames = computed(() =>
+  storeCourseCalendar.newForm.semesters
+    .filter((obj) => obj.name !== '')
+    .map((obj) => obj.name)
+)
 
 const onClickDeleteSemesterButton = (index) => {
   confirm.require({
@@ -164,6 +194,22 @@ const onClickDeleteSemesterButton = (index) => {
       // howevever, the deleted element index position will still return 'empty'
       // so this needs to be handled
       delete storeCourseCalendar.newForm.semesters[index]
+    },
+  })
+}
+
+const onClickDeleteTermButton = (index) => {
+  confirm.require({
+    message:
+      'Are you sure you want to delete this term? Any unsaved changes will be lost.',
+    header: 'Delete Term',
+    icon: 'pi pi-exclamation-triangle',
+    accept: () => {
+      // delete removes the element from the array and preserves the index
+      // we need to preserve the index to ensure selected dates are handled correctly
+      // howevever, the deleted element index position will still return 'empty'
+      // so this needs to be handled
+      delete storeCourseCalendar.newForm.terms[index]
     },
   })
 }
