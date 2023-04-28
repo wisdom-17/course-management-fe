1b43461ae71e774e00605b34144bf041cc2745d8
Saeed <saeed17@gmail.com>
2023-04-28 23:23:45 +0100

    Refactor pagination into separate component

---
+++ src/components/TablePaginator.vue
@@ -0,0 +1,22 @@
+<template>
+  <Paginator
+    :rows="props.rowsPerPage"
+    :totalRecords="props.totalRecords"
+    @page="(e) => $emit('pageChanged', e.page + 1)"
+  ></Paginator>
+</template>
+
+<script setup>
+import Paginator from 'primevue/paginator'
+
+const props = defineProps({
+  totalRecords: {
+    type: Number,
+    default: 0,
+  },
+  rowsPerPage: {
+    type: Number,
+    default: 15,
+  },
+})
+</script>
