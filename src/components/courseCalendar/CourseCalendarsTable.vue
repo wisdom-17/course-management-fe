68ce257e447bb06e42ca08f504ab315f28d0b7a5
Saeed <saeed17@gmail.com>
2023-05-14 22:19:07 +0100

    Show Semester and Course Date details in table

--- src/components/courseCalendar/CourseCalendarsTable.vue
+++ src/components/courseCalendar/CourseCalendarsTable.vue
@@ -16,6 +16,46 @@
       :header="col.header"
       :key="col.field"
     ></Column>
+    <Column header="Semester">
+      <template #body="slotProps">
+        <ul>
+          <li v-for="semester in slotProps.data.semesters" :key="semester.id">
+            {{ semester.name }}
+            <ul v-if="semester.courseDates.length > 0">
+              <li
+                v-for="courseDate in semester.courseDates"
+                :key="courseDate.id"
+                class="mb-3"
+              >
+                {{ courseDate.name }}
+                <br />
+                {{
+                  `${formatDate(new Date(courseDate.startDate))} - ${formatDate(
+                    new Date(courseDate.endDate)
+                  )}`
+                }}
+              </li>
+            </ul>
+            <p v-else><i>No dates in semester</i></p>
+          </li>
+        </ul>
+        <ul>
+          <li
+            v-for="courseDate in slotProps.data.dates"
+            :key="courseDate.id"
+            class="mb-3"
+          >
+            {{ courseDate.name }}
+            <br />
+            {{
+              `${formatDate(new Date(courseDate.startDate))} - ${formatDate(
+                new Date(courseDate.endDate)
+              )}`
+            }}
+          </li>
+        </ul>
+      </template>
+    </Column>
     <Column
       v-for="col in dateValueColumns"
       :field="col.field"
