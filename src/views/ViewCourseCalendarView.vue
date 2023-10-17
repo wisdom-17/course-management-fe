954253317cc825c9d7ea651572878844c2b64f70
Saeed <saeed17@gmail.com>
2023-10-17 22:13:03 +0100

    Dummy content for view course calendar

--- src/views/ViewCourseCalendarView.vue
+++ src/views/ViewCourseCalendarView.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="col courses">
-    <h1>View Course Calendar</h1>
+    <Card class="mt-3">
+      <template #title>View #{{ id }} Course Calendar</template>
+
+      <template #content>Under construction</template>
+    </Card>
   </div>
 </template>
+
+<script setup>
+import Card from 'primevue/card'
+
+defineProps({
+  id: { type: Number, default: 0 },
+})
+</script>
