fff33f86e222a6af527d3fbe0622638e1ff636a1
Saeed <saeed17@gmail.com>
2023-10-31 18:38:08 +0000

    Basic pages for view courses and view course timetable

---
+++ src/views/CourseView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="col courses">
+    <h1>Courses</h1>
+  </div>
+</template>
+
+<script setup></script>
---
+++ src/views/ViewCourseTimetableView.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="col courses">
+    <h1>View #{{ id }} Subject Timetable</h1>
+    <pre>{{ timetable }}</pre>
+  </div>
+</template>
+
+<script setup>
+import { useSubjectStore } from '@/stores/subject'
+import { computed } from 'vue'
+
+const storeSubject = useSubjectStore()
+
+const timetable = computed(() => {
+  return storeSubject.list.data.find((subject) => {
+    return subject.id == props.id
+  })
+})
+
+const props = defineProps({
+  id: { type: String, default: null },
+})
+</script>
