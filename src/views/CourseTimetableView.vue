c447bee583bee4e72430e4c32e178dc7b37f6c37
Saeed <saeed17@gmail.com>
2023-11-04 23:05:52 +0000

    Basic timetable display

--- src/views/CourseTimetableView.vue
+++ src/views/CourseTimetableView.vue
@@ -1,13 +1,30 @@
 <template>
   <div class="col courses">
-    <h1>View #{{ id }} Course </h1>
-
-    <p>Under construction</p>
+    <h1>View #{{ id }} Course Timetable</h1>
+    <div class="grid">
+      <div v-for="(lessons, day) in timetable" :key="day" class="col-12">
+        <h4>{{ day }}</h4>
+        <h5>{{ lessons }}</h5>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup>
-defineProps({
+import { computed, onMounted } from 'vue'
+import { useTimetableStore } from '@/stores/timetable'
+
+const storeTimetable = useTimetableStore()
+
+const timetable = computed(() => storeTimetable.timetable.data)
+
+const props = defineProps({
   id: { type: Number, default: 0 },
+})
+
+onMounted(() => {
+  storeTimetable.getTimetable(props.id)
+
+  console.log(storeTimetable.timetable)
 })
 </script>
