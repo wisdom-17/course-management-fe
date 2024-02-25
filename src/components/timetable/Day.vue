ac185c0d4eef7db67064fd864885341093247475
Saeed <saeed17@gmail.com>
2024-02-25 15:25:44 +0000

    Day component

---
+++ src/components/timetable/Day.vue
@@ -0,0 +1,15 @@
+<template>
+  <Card>
+    <template v-slot:title>
+      <h4>{{ day }}</h4>
+    </template>
+  </Card>
+</template>
+
+<script setup>
+import Card from 'primevue/card'
+
+defineProps({
+  day: { type: String, default: '' },
+})
+</script>
