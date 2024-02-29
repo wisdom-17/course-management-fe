0863f42edef8f1f8afe100f7c171c2da50c09eaf
Saeed <saeed17@gmail.com>
2024-02-29 22:44:23 +0000

    Add progress spinner when timetable is loading

--- src/views/CourseTimetableView.vue
+++ src/views/CourseTimetableView.vue
@@ -2,7 +2,13 @@
   <div class="col courses">
     <h1>View #{{ id }} Course Timetable</h1>
     <TimetableToolbar />
-    <div class="flex flex-row mt-2">
+    <div
+      v-if="storeTimetable.timetable.loading"
+      class="flex justify-content-center mt-2"
+    >
+      <ProgressSpinner />
+    </div>
+    <div v-else class="flex flex-row mt-2">
       <div v-for="(lessons, day) in timetable" :key="day" class="flex-1">
         <Day :day="day" />
         <!-- <h5>{{ lessons }}</h5> -->
@@ -13,6 +19,7 @@
 
 <script setup>
 import { computed, onMounted } from 'vue'
+import ProgressSpinner from 'primevue/progressspinner'
 import { useTimetableStore } from '@/stores/timetable'
 import Day from '@/components/timetable/Day.vue'
 import TimetableToolbar from '../components/timetable/TimetableToolbar.vue'
